9e8cf7e55ac8059d9789cad4ded5514f68d1b59f
jtquangduy <justinquangduy@gmail.com>
2024-12-24 11:08:21 +0700

    Injecting Other Values (NOT Services)

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,8 +1,8 @@
-import { TaskStatus } from './../task.model';
 import { Component, computed, inject, signal } from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksServiceToken } from '../../../main';
+import { TASK_STATUS_OPTIONS, taskStatusOptionsProvider } from '../task.model';
 
 @Component({
   selector: 'app-tasks-list',
@@ -10,11 +10,14 @@ import { TasksServiceToken } from '../../../main';
   templateUrl: './tasks-list.component.html',
   styleUrl: './tasks-list.component.css',
   imports: [TaskItemComponent],
+  providers: [taskStatusOptionsProvider],
 })
 export class TasksListComponent {
   private tasksService = inject(TasksServiceToken);
 
   selectedFilter = signal<string>('all');
+
+  taskStatusOptions = inject(TASK_STATUS_OPTIONS);
 
   tasks = computed(() => {
     switch (this.selectedFilter()) {
